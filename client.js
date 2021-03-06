ea5f13ad09a94bcf4d1ba101c6cc318953d4d5d7
Nathan Marks <nathan.marks@instacart.com>
2021-03-06 14:36:20 -0500

    repro issue

--- client.js
+++ client.js
@@ -1,34 +1,52 @@
 /** @jsx jsx */
-import * as React from 'react';
-import { jsx, CacheProvider, css, Global } from '@emotion/core';
-import createCache from '@emotion/cache';
-import ReactDOM from 'react-dom';
+import * as React from "react";
+import { jsx, CacheProvider, css, Global } from "@emotion/core";
+import createCache from "@emotion/cache";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 /* Manually kept in sync with server.js for now */
 function App() {
-    return (
-        <>
-            <Global
-                styles={css`
-                    .foo {
-                        color: red;
-                    }
-                `}
-            />
-            <h1 className="foo" css={{ color: 'green' }}>
-                Hello, world!
-            </h1>
-            <p>View this page with javascript enabled and disabled. The text above should be green.</p>
-        </>
-    );
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <div>
+          <Global
+            styles={css`
+              body {
+                background-color: red;
+                color: white;
+              }
+            `}
+          />
+          <p>This should have a red background and white text</p>
+          <p>
+            <Link to="/not-red" css={{ color: "black" }}>
+              Not Red Page
+            </Link>
+          </p>
+        </div>
+      </Route>
+      <Route path="/not-red" exact>
+        <div>
+          <p>This should NOT have a red background or white text</p>
+          <p>
+            <Link to="/">Red Page</Link>
+          </p>
+        </div>
+      </Route>
+    </Switch>
+  );
 }
 
 const cache = createCache();
-let element = (
-    <CacheProvider value={cache}>
-        <App />
-    </CacheProvider>
-);
 
-ReactDOM.hydrate(element, document.getElementById('root'));
-console.log('Hydrated!');
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <CacheProvider value={cache}>
+      <App />
+    </CacheProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+console.log("Hydrated!");
--- server.js
+++ server.js
@@ -1,54 +1,73 @@
 /** @jsx jsx */
-const React = require('react');
-const { css, Global, jsx, CacheProvider } = require('@emotion/core');
-const { renderToString } = require('react-dom/server');
-const createEmotionServer = require('create-emotion-server').default;
-const createCache = require('@emotion/cache').default;
-const express = require('express');
-const path = require('path');
+const React = require("react");
+const { css, Global, jsx, CacheProvider } = require("@emotion/core");
+const { renderToString } = require("react-dom/server");
+const createEmotionServer = require("create-emotion-server").default;
+const createCache = require("@emotion/cache").default;
+const express = require("express");
+const path = require("path");
+const { StaticRouter, Switch, Route, Link } = require("react-router-dom");
 
-/* Manually kept in sync with server.js for now */
+/* Manually kept in sync with client.js for now */
 function App() {
-    return (
-        <>
-            <Global
-                styles={css`
-                    .foo {
-                        color: red;
-                    }
-                `}
-            />
-            <h1 className="foo" css={{ color: 'green' }}>
-                Hello, world!
-            </h1>
-            <p>View this page with javascript enabled and disabled. The text above should be green.</p>
-        </>
-    );
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <div>
+          <Global
+            styles={css`
+              body {
+                background-color: red;
+                color: white;
+              }
+            `}
+          />
+          <p>This should have a red background and white text</p>
+          <p>
+            <Link to="/not-red" css={{ color: "black" }}>
+              Not Red Page
+            </Link>
+          </p>
+        </div>
+      </Route>
+      <Route path="/not-red" exact>
+        <div>
+          <p>This should NOT have a red background or white text</p>
+          <p>
+            <Link to="/">Red Page</Link>
+          </p>
+        </div>
+      </Route>
+    </Switch>
+  );
 }
-
-const cache = createCache();
-const { extractCritical } = createEmotionServer(cache);
-
-let element = (
-    <CacheProvider value={cache}>
-        <App />
-    </CacheProvider>
-);
-
-let { html, css: cssContent, ids } = extractCritical(renderToString(element));
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    const resp = `<!DOCTYPE html>
+app.get("/", (req, res) => {
+  const cache = createCache();
+  const { extractCritical } = createEmotionServer(cache);
+
+  const element = (
+    <StaticRouter>
+      <CacheProvider value={cache}>
+        <App />
+      </CacheProvider>
+    </StaticRouter>
+  );
+
+  const { html, css: cssContent, ids } = extractCritical(
+    renderToString(element)
+  );
+  const resp = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My site</title>
-    <style data-emotion-css="${ids.join(' ')}">${cssContent}</style>
+    <style data-emotion-css="${ids.join(" ")}">${cssContent}</style>
 </head>
 <body>
     <div id="root">${html}</div>
@@ -56,14 +75,14 @@ app.get('/', (req, res) => {
     <script src="./bundle.js"></script>
 </body>
 </html>`;
-    console.log(resp);
-    res.send(resp);
+  console.log(resp);
+  res.send(resp);
 });
 
-app.get('/bundle.js', (req, res) => {
-    res.sendFile(path.resolve('bundle.js'));
+app.get("/bundle.js", (req, res) => {
+  res.sendFile(path.resolve("bundle.js"));
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
